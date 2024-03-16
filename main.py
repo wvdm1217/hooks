a8429d00dc7a53f26c5e4c2a29d6c41cbd31756c
Werner van der Merwe <wvdm1217@gmail.com>
2024-03-16 15:23:16 +0000

    fix: pre-commit githook

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+# main.py for testing python githooks
+
+
 def reverse_string(string):
     return string[::-1]
 
 
 if __name__ == "__main__":
     print(reverse_string("hello world"))
-    print(reverse_string("goodbye world"))
     print(reverse_string("hello world") == "dlrow olleh")
