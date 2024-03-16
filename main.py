b025219f0c7e2c899c4d6765e2482cd49acf74bd
Werner van der Merwe <wvdm1217@gmail.com>
2024-03-16 15:13:22 +0000

    fix: remove other hooks from pre-commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def reverse_string(string):
     return string[::-1]
 
+
 if __name__ == "__main__":
     print(reverse_string("hello world"))
+    print(reverse_string("goodbye world"))
     print(reverse_string("hello world") == "dlrow olleh")
